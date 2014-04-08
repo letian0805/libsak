099b952c0e1b0841171bce56df75130a969e7084
Yi Letian <yilt@vcnglobal.com>
2014-04-08 14:57:46 +0800

    modified:   test/tokbkt_test.c

--- test/tokbkt_test.c
+++ test/tokbkt_test.c
@@ -4,12 +4,33 @@
 
 #include "token_bucket.h"
 
+typedef struct{
+    char *msg0;
+    char *msg1;
+}TBTest;
+
+int tb_callback(TokenBucket *tb, int state, void *user_data)
+{
+    TBTest *test = (TBTest *)user_data;
+    if (state == TB_STATE_UNLOCK){
+        printf("-----tb_callback: %s\n", test->msg0);
+    }else{
+        printf("-----tb_callback: %s\n", test->msg1);
+    }
+
+    return 0;
+}
+
 int main(void)
 {
     TokenBucketManager *tbman = token_bucket_manager_create(6, 1);
     int bitrate = 1024*1024;
+    TBTest *test = (TBTest *)malloc(sizeof(TBTest));
+    test->msg0 = "unlocked";
+    test->msg1 = "locked";
     TokenBucket *tb = token_bucket_create(bitrate);
     token_bucket_set_manager(tb, tbman);
+    token_bucket_set_state_callback(tb, tb_callback, (void *)test);
     struct timeval tm1;
     struct timeval tm2;
     srand(time(NULL));
@@ -26,8 +47,8 @@ int main(void)
     gettimeofday(&tm2, NULL);
     double time = tm2.tv_sec - tm1.tv_sec + (tm2.tv_usec - tm1.tv_usec)/1000000.0;
     double speed = (double)sum/time;
+    usleep(100000);
     printf("--------time: %lfS, sum: %d\n", time, sum);
-    token_bucket_destroy(tb);
 
     if (speed >= 1024*1024){
         printf("------speed is: %lf Mb/s\n", speed/(1024*1024));
@@ -42,6 +63,7 @@ int main(void)
     }else{
         printf("----speed deviation: %.2f%%, %db/s\n", (100.0 * deviation) / bitrate, deviation);
     }
+    token_bucket_destroy(tb);
 
     return 0;
 }
